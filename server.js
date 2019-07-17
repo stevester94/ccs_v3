12a388a1565bcc93807ebc5ae2c463c3a2685285
Steven Mackey <stevester94@gmail.com>
2019-07-16 20:59:17 -0700

    Uber basic websocket example

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+
+// Websocket server portion
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 8081 });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
+});
+
+
+// Static server portion
 var express = require('express');
 var app = express();
-
 
 app.use(express.static('public'));
 
@@ -12,3 +27,5 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
+
