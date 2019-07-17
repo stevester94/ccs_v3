efde5db06907cd291a037f64d02a68f36b7e4745
Steven Mackey <stevester94@gmail.com>
2019-07-16 22:28:58 -0700

    Websocket shenannigans

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-
 // Websocket server portion
 const WebSocket = require('ws');
  
-const wss = new WebSocket.Server({ port: 8081 });
- 
-wss.on('connection', function connection(ws) {
+
+const receiver_socket = new WebSocket.Server({ port: 8082 });
+const sender_socket   = new WebSocket.Server({ port: 8081 });
+
+// The sender will broadcast to all receivers 
+sender_socket.on('connection', function connection(ws) {
+  console.log("A sender has connected");
+
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    console.log('received: %s on sender socket', message);
+
+    receiver_socket.clients.forEach(function(client) {
+      console.log("  Relaying to receiver");
+      client.send(message);
+    });
   });
- 
-  ws.send('something');
+});
+
+receiver_socket.on('connection', function connection(ws) {
+  console.log("A receiver has connected");
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s on receiver socket', message);
+
+    // Yeah it's fucky, I just want to see it work
+    sender_socket.clients.forEach(function(client) {
+      console.log("  Relaying to sender");
+      client.send(message);
+    });
+  });
 });
 
 
@@ -18,11 +39,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    console.log("Serving index.html");
-    res.sendFile("index.html");
-});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
